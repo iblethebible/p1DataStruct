4b1d91c6422aa1aead7b903424eaffc061af71a5
Harry Morris <iblethebible@zoho.com>
2022-12-05 23:31:16 +0000

    updated with constructors

--- p1DataStruct/Program.cs
+++ p1DataStruct/Program.cs
@@ -1,19 +1,35 @@
-﻿namespace p1DataStruct
+﻿using System.Runtime.InteropServices;
+
+namespace p1DataStruct
 {
     internal class Program
     {
+
+        //method to end program
         static void EndProgram()
         {
             Console.WriteLine("Press anykey to end program");
             Console.ReadKey();
         }
+       
+        //creat structure for team
         struct Team
         {
             public string name;
             public string dateEstablished;
             public int tier;
             public int positioninleague;
+            //create constructor for team
+            public Team(string name, string dateEstablished, int tier, int posiitioninleague)
+            {
+                this.name = name;
+                this.dateEstablished = dateEstablished;
+                this.tier = tier;
+                this.positioninleague = posiitioninleague;
+            }
         }
+
+        //create struct for player
         struct Player
         {
             public string team;
@@ -21,24 +37,32 @@
             public string name;
             public string dob;
             public string position;
+
+
+            //constructor for player
+            public Player(string team, int shirtNumber, string name, string dob, string position)
+            {
+                this.team = team;
+                this.shirtNumber = shirtNumber;
+                this.name = name;
+                this.dob = dob;
+                this.position = position;
+            }
         }
         static void Main(string[] args)
         {
-            Team t1 = new Team();
-            t1.name = "AFCCrewe";
-            t1.dateEstablished = "02/02/2022";
-            t1.tier = 14;
-            t1.positioninleague = 1;
 
-            Player p1 = new Player();
-            p1.name = "Tom Laight";
-            p1.shirtNumber = 7;
-            p1.team = "AFCCrewe";
-            p1.dob = "02/02/2001";
-            p1.position = "CDM";
+            //create team with constructor
+            Team t1 = new Team("AFCCrewe", "02/02/02", 14, 1);
+
+            //create player with constructor
+            Player p1 = new Player("AFCCrewe", 7, "Tom Laight", "02/02/02", "CDM");
+            Player p2 = new Player("AFCCrewe", 8, "Ronaldo", "02/02/95", "Striker");
+
+            
 
 
-            Console.WriteLine(t1.name);
+            Console.WriteLine(p1.name);
 
             EndProgram();
         }
